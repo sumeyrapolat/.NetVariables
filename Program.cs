d45d833775904fa09d049482956b31ae1fb6b374
SumeyraPolatt <180315055@ogr.cbu.edu.tr>
2024-03-31 01:02:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your surname: ");
+string surname = Console.ReadLine();
+
+Console.WriteLine($"Hello {name} {surname}.");
+
+DateTime dateTime = DateTime.Now;
+Console.WriteLine(dateTime);
+
+int number = 20;
+string number2 = "20";
+Console.WriteLine(number + number2.ToString());
+
+string number3 = number2.ToString();
+
+int number4 = Convert.ToInt32(number3);
+Console.WriteLine(number4);
+
+string dateTime2 = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(dateTime2);
+
+string dateTime3 = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine(dateTime3);
+
+string hour = DateTime.Now.ToString("HH:mm");
+Console.WriteLine(hour);
